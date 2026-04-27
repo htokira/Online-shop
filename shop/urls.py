b4eb63c642241db99cfc777bca19bf410e34c358
Drehel Ivanna <drehelivanka@knu.ua>
2026-04-28 02:56:45 +0300

    Add URLs for incrementing and decrementing cart item quantity

--- shop/urls.py
+++ shop/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('cart/', views.cart_detail, name='cart_detail'), # Твій кошик
+    path('cart/add-one/<int:product_id>/', views.cart_add_one, name='cart_add_one'),
+    path('cart/remove-one/<int:product_id>/', views.cart_remove_one, name='cart_remove_one'),
 ]
