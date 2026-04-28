913c5a525b4545af6243a7ba9cfb5016f796ed70
Drehel Ivanna <drehelivanka@knu.ua>
2026-04-28 04:01:39 +0300

    "Add Order and OrderItem models with OrderCreateForm"

--- shop/forms.py
+++ shop/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Order
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +9,7 @@ class UserRegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('email',)
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'address'] # Поля, які ми хочемо від юзера
