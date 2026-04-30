9612cbf4f1a257f452b721fccb6357d9abf46c31
Kira Mikhoparova <mihoparovakira@gmail.com>
2026-04-30 22:33:32 +0300

    Add phone validation

--- shop/forms.py
+++ shop/forms.py
@@ -1,11 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from .models import Order, Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    phone_number = forms.CharField(max_length=20, required=True, label="Номер телефону")
+
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone must be in format: '+999999999'."
+    )
+    
+    phone_number = forms.CharField(
+        validators=[phone_regex], 
+        max_length=17, 
+        required=True, 
+        label="Номер телефону"
+    )
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -13,10 +25,8 @@ class UserRegisterForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super().save(commit=commit)
-        Profile.objects.create(
-            user=user,
-            phone_number=self.cleaned_data.get('phone_number')
-        )
+        phone = self.cleaned_data.get('phone_number')
+        Profile.objects.create(user=user, phone_number=phone)
         return user
 
 class OrderCreateForm(forms.ModelForm):
