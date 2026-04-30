58a3583e29b970bedd9ff51bf121d71d7a4a8f3d
Kira Mikhoparova <mihoparovakira@gmail.com>
2026-04-30 21:36:29 +0300

    Fix register form

--- shop/forms.py
+++ shop/forms.py
@@ -1,14 +1,24 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Order
+from .models import Order, Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    phone_number = forms.CharField(max_length=20, required=True, label="Номер телефону")
 
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('email',)
+
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+        Profile.objects.create(
+            user=user,
+            phone_number=self.cleaned_data.get('phone_number')
+        )
+        return user
+
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
